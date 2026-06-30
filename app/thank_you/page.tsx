170c4299b7366cd7403a93569f050c07b85ec0cc
GeeBeeScript <gbemigashodimu@gmail.com>
2026-06-30 11:11:28 +0100

    thank you page styling

--- app/thank_you/page.tsx
+++ app/thank_you/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import OtherNav from "../[components]/OtherNav";
+
 
 const ThankYouPage = () => {
   return (
-    <div>Thank you very much. We look forward to getting in contact with you</div>
+    <main>
+      <OtherNav />
+      <div className="w-full min-h-screen flex justify-center items-center font-bold font-main text-3xl text-center text-gold-section-color/70 max-[400px]:break-all">
+        Thank you very much. We look forward to getting in contact with you
+      </div>
+    </main>
   )
 }
 
